b3a49c62fb4fe14c22bd806a3d492880094dc609
Kohe Tokoi <tokoik@gmail.com>
2026-05-05 01:29:20 +0900

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,6 +15,12 @@
 #include <stdlib.h>
 #include <math.h>
 
+/* トラックボール処理用関数の宣言 */
+#include "trackball.h"
+
+/* 箱を描く関数の宣言 */
+#include "box.h"
+
 /*
 ** 光源
 */
@@ -85,9 +91,6 @@ static void init(void)
   glLightfv(GL_LIGHT0, GL_AMBIENT, lightamb);
 }
 
-/* 箱を描く関数の宣言 */
-#include "box.h"
-
 /*
 ** シーンの描画
 */
@@ -111,9 +114,6 @@ static void scene(void)
 /****************************
 ** GLUT のコールバック関数 **
 ****************************/
-
-/* トラックボール処理用関数の宣言 */
-#include "trackball.h"
 
 static void display(void)
 {
